df458554359863faab899d391ca60716d37b4a8b
frnmst/Franco Masotti <franco.masotti@live.com>
2017-05-17 15:31:30 +0200

    Fixed bst_same_keys.py.

--- algorithms/data_structures/bst_same_keys.py
+++ algorithms/data_structures/bst_same_keys.py
@@ -18,7 +18,7 @@ import random
 import timeit
 from bst import Bst, BstNode
 
-# From exercise 12.1
+# From exercise 12-1
 
 class BstSameKeysNode(BstNode):
     def __init__(self,key,right=None,left=None,parent=None):
@@ -42,31 +42,22 @@ class BstSameKeys(Bst):
         else:
             return root
 
-    def insert_based_on_b():
-        pass
-
-    def insert_in_list():
-        pass
-
-    def insert_randomly(self,key,root=None):
+    # Not sure about this one: putting x.b = ...
+    # in the while loop leads to an is_bst() method
+    # failure. So following is the possible right answer.
+    def insert_based_on_b(self,key,root=None):
         x = self.__set_root(root)
         y = None
         z = BstSameKeysNode(key)
-        bool = random.choice([True, False])
 
         # Find correct position for node z.
         while x is not None:
             y = x
-            if z.key < x.key:
+            assert x.key == z.key
+            if x.b is True:
                 x = x.left
-            elif z.key > x.key:
-                x = x.right
             else:
-                assert x.key == z.key
-                if bool is True:
-                    x = x.left
-                else:
-                    x = x.right
+                x = x.right
 
         z.parent = y
 
@@ -77,10 +68,60 @@ class BstSameKeys(Bst):
         if y is None:
             self.root = z
         # Parent vertex connects to son.
-        elif z.key < y.key:
-            y.left = z
-        elif z.key > y.key:
-            y.right = z
+        # Change the boolean of the parent only.
+        else:
+            assert z.key == y.key
+            if y.b is True:
+                y.b = False
+                y.left = z
+            else:
+                y.b = True
+                y.right = z
+
+        self.nodes += 1
+
+
+    #######
+    #######
+    #######
+    #######
+
+
+    # "Keep a list of nodes with equal keys at x, and insert z into the list."
+    def insert_in_list():
+        pass
+
+
+    #######
+    #######
+    #######
+    #######
+
+
+    def insert_randomly(self,key,root=None):
+        x = self.__set_root(root)
+        y = None
+        z = BstSameKeysNode(key)
+        bool = random.choice([True, False])
+
+        # Find correct position for node z.
+        while x is not None:
+            y = x
+            assert x.key == z.key
+            if bool is True:
+               x = x.left
+            else:
+               x = x.right
+
+        z.parent = y
+
+        if y is not None and x is not None:
+            assert x.parent is y
+
+        # Was an empty tree.
+        if y is None:
+            self.root = z
+        # Parent vertex connects to son.
         else:
             assert z.key == y.key
             if bool is True:
@@ -101,7 +142,15 @@ def test_a(samples,key):
     assert t.is_bst()
 
 def test_b(samples,key):
-    pass
+    t = None
+
+    t = BstSameKeys(key)
+
+    for s in range(1,samples):
+        t.insert_based_on_b(key)
+
+    assert t.is_bst()
+
 
 def test_c(samples,key):
     pass
@@ -123,7 +172,13 @@ def test_d(samples,key):
 def test(tests,verbose=False):
     SAMPLES = 1000
     key = 15
-    test_names = [ "a", "b", "c", "d" ]
+    test_names = [ ]
+
+    # You can comment out unwanted tests here.
+    test_names.append("a")
+    test_names.append("b")
+    # test_names.append("c")
+    test_names.append("d")
     benchmarks = dict()
 
     for t in test_names:
@@ -133,18 +188,18 @@ def test(tests,verbose=False):
         benchmarks[t]=b
 
     for t in test_names:
-        print(t + ": " + str(benchmarks[t] + " seconds"))
+        print(t + ": " + str(benchmarks[t]) + " seconds")
 
-    assert benchmarks['a'] > benchmarks['b']
-    assert benchmarks['a'] > benchmarks['c']
-    assert benchmarks['a'] > benchmarks['d']
+    if 'a' in benchmarks:
+        assert benchmarks['a'] > benchmarks['b']
+        assert benchmarks['a'] > benchmarks['d']
 
 # Run n tests and check that the n lists returned are all equal to each other
 # knowing that they have been generated randomly, they have the same size
 # and the same unique keys. This means that the MAX - MIN + 1 numbers generated
 # above are instered in the data structure randomly.
 if __name__ == '__main__':
-    TESTS = 500
+    TESTS = 50
 
     test(TESTS)
     print ("All tests passed")
